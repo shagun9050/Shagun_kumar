53a8b332ae5b4af1ad4674b1577cea2136cb885f
shagun9050 <sagunkumar0123@gmail.com>
2025-12-29 18:05:18 +0000

    container cong

---
+++ javascript/datatype.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+console.log(3 
+    + 3); // code readablity should be high
+
+console.log("hitesh")
+
+let name = "Shagun"
+let age = 20
+let islogIn = false
+
+// number => 2 to power 53
+// null => standlone value
+// symbol => unique
+
+console.log(typeof "Shagun");
+console.log(typeof 25);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
