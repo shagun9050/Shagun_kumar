54f7f6848cbbfba7ebc347f0444eaa7600713688
shagun9050 <sagunkumar0123@gmail.com>
2025-12-29 15:03:15 +0000

    container cong

---
+++ javascript/variable.js
@@ -0,0 +1,14 @@
+const accountId = 123456
+let accountName = "Shagun_kumar"
+var accountPassword = "1265"
+accountCity = "Jaipur"
+
+accountCity = "haryana"
+accountPassword = "656859"
+accountName = "Shagun"
+
+/* not to prefer to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId ,accountCity ,accountPassword ,accountName]);
